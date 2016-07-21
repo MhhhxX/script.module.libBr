a0a1bd548ca0edd926c7ad8f1c208d4e88493667
prof-membrane <pr0lckr@googlemail.com>
2016-07-21 15:26:03 +0200

    added support for a standalone add-on

--- lib/libBr.py
+++ lib/libBr.py
@@ -3,16 +3,87 @@ import sys
 import xbmc
 import xbmcgui
 import xbmcplugin
+import xbmcaddon
 import urllib
 import libBrJsonParser
+import libMediathek
+from datetime import date, timedelta
+
+translation = xbmcaddon.Addon(id='script.module.libMediathek').getLocalizedString
+
+
+
+
 
 def getDate(date):
-	return listing.parseDate(date)
+	return libBrJsonParser.parseDate(date)
 def getVideoUrl(url):
 	return libBrJsonParser.parseVideo(url)
-	
+def play(dict):
+	url = getVideoUrl(dict["url"])
+	#listitem = xbmcgui.ListItem(label=video["name"],thumbnailImage=video["thumb"],path=url)
+	listitem = xbmcgui.ListItem(label=dict["name"],path=url)
+	xbmc.Player().play(url, listitem)	
 	
 
 def libBrPvrPlay(dict):
 	import libBrPvr
 	libBrPvr.play(dict)
+	
+def libBrListMain():
+	libMediathek.addEntry({'name':translation(31032), 'mode':'libBrListLetters'})
+	libMediathek.addEntry({'name':translation(31033), 'mode':'libBrListDate'})
+	#libMediathek.addEntry({'name':translation(31034), 'mode':'libArdListVideos', 'url':'http://www.ardmediathek.de/appdata/servlet/tv/Rubriken/mehr?documentId=21282550&json'})
+	
+	
+	
+def libBrListLetters():
+	libMediathek.populateDirAZ('libBrListShows')
+	
+def libBrListShows():
+	libMediathek.addEntries(libBrJsonParser.parseShows(params['name']))
+	
+def libBrListVideos():
+	libMediathek.addEntries(libBrJsonParser.parseVideos(params['url']))
+
+def libBrListDate():
+	libMediathek.populateDirDate('libBrListDateChannels')
+	
+def libBrListDateChannels():
+	xbmc.log(str(params))
+	libMediathek.addEntry({'name':'ARD-Alpha', 'mode':'libBrListDateVideos', 'datum': params['datum']})
+	libMediathek.addEntry({'name':'BR', 'mode':'libBrListDateVideos', 'datum': params['datum']})
+
+def libBrListDateVideos():
+	datum = date.today() - timedelta(int(params['datum']))
+	xbmc.log(datum.strftime('%Y-%m-%d'))
+	libMediathek.addEntries(libBrJsonParser.parseDate(datum.strftime('%Y-%m-%d'),params['name']))#params['datum'] =yyyy-mm-dd
+	
+def libBrPlay():
+	url = libBrJsonParser.parseVideo(params['url'])
+	xbmc.log(url)
+	listitem = xbmcgui.ListItem(path=url)
+	xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
+	
+	
+def list():	
+	modes = {
+	'libBrListLetters': libBrListLetters,
+	'libBrListShows': libBrListShows,
+	'libBrListVideos': libBrListVideos,
+	'libBrListDate': libBrListDate,
+	'libBrListDateChannels': libBrListDateChannels,
+	'libBrListDateVideos': libBrListDateVideos,
+	'libBrPlay': libBrPlay
+	}
+	global params
+	params = libMediathek.get_params()
+	global pluginhandle
+	pluginhandle = int(sys.argv[1])
+	
+	if not params.has_key('mode'):
+		libBrListMain()
+	else:
+		modes.get(params['mode'],libBrListMain)()
+	
+	xbmcplugin.endOfDirectory(int(sys.argv[1]))	
--- lib/libBrJsonParser.py
+++ lib/libBrJsonParser.py
@@ -2,29 +2,80 @@
 import xbmc
 import json
 import _utils
+#import dateutil.parser
 
 pluginpath = 'plugin://script.module.libArd/'
-chan = {"br":"channel_28107"}
+chan = {"BR":"channel_28107",
+		"br":"channel_28107",
+		"ARD-Alpha":"channel_28487",
+		"ardalpha":"channel_28487"}
 
-def parseDate(date):
-	c = "br"#TODO
+def _parseMain():
 	response = _utils.getUrl("http://www.br.de/system/halTocJson.jsp")
 	j = json.loads(response)
 	url = j["medcc"]["version"]["1"]["href"]
 	response = _utils.getUrl(url)
+	return json.loads(response)
+	
+def parseShows(letter):
+	j = _parseMain()
+	url = j["_links"]["broadcastSeriesAz"]["href"]
+	response = _utils.getUrl(url)
 	j = json.loads(response)
-
-	xbmc.log(str(j))
+	url = j['az']['_links'][letter.lower()]['href']
+	response = _utils.getUrl(url)
+	j = json.loads(response)
+	list = []
+	for show in j["_embedded"]["teasers"]:
+		dict = {}
+		dict['url'] = show["_links"]["self"]["href"]
+		dict['name'] = show["headline"]
+		dict['subtitle'] = show["topline"]
+		if 'br-core:teaserText' in show["documentProperties"]:
+			dict['plot'] = show["documentProperties"]["br-core:teaserText"]
+		#dict['thumb'] = show["teaserImage"]["_links"]["image480q"]["href"]
+		
+		dict['type'] = 'dir'
+		dict['mode'] = 'libBrListVideos'
+		
+		list.append(dict)
+	return list
+	
+def parseVideos(url):
+	response = _utils.getUrl(url)
+	j = json.loads(response)
+	url = j["_links"]["latestVideos"]["href"]
+	response = _utils.getUrl(url)
+	j = json.loads(response)
+	list = []
+	for show in j["_embedded"]["teasers"]:
+		dict = {}
+		dict['url'] = show["_links"]["self"]["href"]
+		dict['name'] = show["headline"]
+		dict['subtitle'] = show["topline"]
+		dict['plot'] = show["teaserText"]
+		dict['thumb'] = show["teaserImage"]["_links"]["image480q"]["href"]
+		
+		dict['type'] = 'video'
+		dict['mode'] = 'libBrPlay'
+		
+		list.append(dict)
+	return list
+	
+def parseDate(date,channel='BR'):
+	j = _parseMain()
+	#xbmc.log(str(j))
 	url = j["_links"]["epg"]["href"]
 	response = _utils.getUrl(url)
 	j = json.loads(response)
 	url = j["epgDays"]["_links"][date]["href"]#date: 2016-12-30
 	response = _utils.getUrl(url)
 	j = json.loads(response)
-	
+	#xbmc.log(str(j))
 	list = []
-	broadcasts = j["channels"][chan[c]]["broadcasts"]
+	broadcasts = j["channels"][chan[channel]]["broadcasts"]
 	for b in broadcasts:
+		xbmc.log(str(b))
 		if "_links" in b and "video" in b["_links"]:
 			dict = {}
 			
@@ -34,6 +85,8 @@ def parseDate(date):
 			dict["url"] = b["_links"]["video"]["href"]
 			dict["time"] = startTimeToInt(b["broadcastStartDate"][11:19])
 			#TODO: rest of properties
+			dict['type'] = 'video'
+			dict['mode'] = 'libBrPlay'
 			list.append(dict)
 	return list
 			
@@ -49,7 +102,9 @@ def parseVideo(url):
 	assets = j["assets"]
 	for asset in assets:
 		if "type" in asset and asset["type"] == "HLS_HD":
-			#xbmc.log(asset["_links"]["stream"])
+			return asset["_links"]["stream"]["href"]
+	for asset in assets:
+		if "type" in asset and asset["type"] == "HLS":
 			return asset["_links"]["stream"]["href"]
 			
 def startTimeToInt(s):
